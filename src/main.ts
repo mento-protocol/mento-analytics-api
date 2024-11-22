c242827377f0f9091212a6d9813bb02984cdf347
Bayological <6872903+bayological@users.noreply.github.com>
2024-11-22 14:41:06 +0700

    docs: enable swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mento Analytics API')
+    .setDescription('API for analytics data on the Mento Protocol')
+    .setVersion('1.0')
+    .addTag('stablecoins')
+    .addTag('reserve')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3255);
 }
 bootstrap();
