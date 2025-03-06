8b6eed94ef5cdd3585f3afbeaca2c300639d5af3
Bayological <6872903+bayological@users.noreply.github.com>
2025-03-06 10:07:48 -0500

    chore: add comma

--- src/api/reserve/config/assets.config.ts
+++ src/api/reserve/config/assets.config.ts
@@ -112,7 +112,7 @@ export const ASSETS_CONFIGS: Record<Chain, Partial<Record<AssetSymbol, AssetConf
       symbol: 'USDT',
       name: 'USD Tether',
       decimals: 6,
-      address: '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+      address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
     },
   },
   [Chain.BITCOIN]: {
