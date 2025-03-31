16fda88abf81f95a1b47fbb6d1bdd3042beacf57
Bayological <6872903+bayological@users.noreply.github.com>
2025-03-31 15:44:13 -0500

    refactor: set up public clients

--- src/common/services/chain-provider.service.ts
+++ src/common/services/chain-provider.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { JsonRpcProvider, Provider } from 'ethers';
+import { createPublicClient, webSocket, PublicClient } from 'viem';
 import { ConfigService } from '@nestjs/config';
 import { Chain } from '@types';
+import { celo, mainnet } from 'viem/chains';
 
 @Injectable()
-export class ChainProvidersService {
-  private providers: Map<Chain, Provider> = new Map();
+export class ChainClientService {
+  private clients: Map<Chain, any> = new Map();
 
   constructor(private config: ConfigService) {
     this.initializeProviders();
@@ -22,20 +23,26 @@ export class ChainProvidersService {
       throw new Error('ETH_RPC_URL is not set');
     }
 
-    this.providers.set(Chain.CELO, new JsonRpcProvider(celoRpcUrl, 4220, { staticNetwork: true }));
-    this.providers.set(Chain.ETHEREUM, new JsonRpcProvider(ethereumRpcUrl, 1, { staticNetwork: true }));
+    const celoClient = createPublicClient({
+      chain: celo,
+      transport: webSocket(celoRpcUrl),
+    });
+    this.clients.set(Chain.CELO, celoClient);
+
+    const ethereumClient = createPublicClient({ chain: mainnet, transport: webSocket(ethereumRpcUrl) });
+    this.clients.set(Chain.ETHEREUM, ethereumClient);
   }
 
   /**
-   * Get a provider for a given chain
-   * @param chain - The chain to get the provider for
-   * @returns The provider for the chain
+   * Get a client for a given chain
+   * @param chain - The chain to get the client for
+   * @returns The client for the chain
    */
-  getProvider(chain: Chain): Provider {
-    const provider = this.providers.get(chain);
-    if (!provider) {
-      throw new Error(`No provider available for chain ${chain}`);
+  getClient(chain: Chain): PublicClient {
+    const client = this.clients.get(chain);
+    if (!client) {
+      throw new Error(`No client available for chain ${chain}`);
     }
-    return provider;
+    return client;
   }
 }
