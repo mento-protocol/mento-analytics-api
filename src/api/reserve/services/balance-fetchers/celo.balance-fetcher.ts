974b20fddca11a4255b6b7a54b3898d6ce56988b
Bayological <6872903+bayological@users.noreply.github.com>
2025-04-01 16:31:32 -0500

    feat: refactor caching & change transport (#48)

--- src/api/reserve/services/balance-fetchers/celo.balance-fetcher.ts
+++ src/api/reserve/services/balance-fetchers/celo.balance-fetcher.ts
@@ -3,8 +3,8 @@ import { Injectable, Logger } from '@nestjs/common';
 import { AddressCategory, Chain } from '@types';
 import { BalanceFetcherConfig, BaseBalanceFetcher } from '.';
 import { ERC20BalanceFetcher } from './erc20-balance-fetcher';
-import { ChainProvidersService } from '@common/services/chain-provider.service';
-import { EthersAdapter, UniV3SupplyCalculator } from '@mento-protocol/mento-sdk';
+import { ChainClientService } from '@/common/services/chain-client.service';
+import { ViemAdapter, UniV3SupplyCalculator } from '@mento-protocol/mento-sdk';
 import { UNIV3_POSITION_MANAGER_ADDRESS, UNIV3_FACTORY_ADDRESS } from '../../constants';
 import * as Sentry from '@sentry/nestjs';
 @Injectable()
@@ -12,13 +12,13 @@ export class CeloBalanceFetcher extends BaseBalanceFetcher {
   private readonly logger = new Logger(CeloBalanceFetcher.name);
   private readonly erc20Fetcher: ERC20BalanceFetcher;
 
-  constructor(private readonly chainProviders: ChainProvidersService) {
+  constructor(private readonly chainClientService: ChainClientService) {
     const config: BalanceFetcherConfig = {
       chain: Chain.CELO,
       supportedCategories: [AddressCategory.MENTO_RESERVE, AddressCategory.UNIV3_POOL],
     };
     super(config);
-    this.erc20Fetcher = new ERC20BalanceFetcher(this.chainProviders.getProvider(Chain.CELO));
+    this.erc20Fetcher = new ERC20BalanceFetcher(this.chainClientService.getClient(Chain.CELO));
   }
 
   async fetchBalance(tokenAddress: string | null, accountAddress: string, category: AddressCategory): Promise<string> {
@@ -54,7 +54,7 @@ export class CeloBalanceFetcher extends BaseBalanceFetcher {
 
   private async fetchUniv3PoolBalance(tokenAddress: string, accountAddress: string): Promise<string> {
     try {
-      const adapter = new EthersAdapter(this.chainProviders.getProvider(Chain.CELO));
+      const adapter = new ViemAdapter(this.chainClientService.getClient(Chain.CELO));
       const calculator = new UniV3SupplyCalculator(
         adapter,
         UNIV3_POSITION_MANAGER_ADDRESS,
