cc00b1ccee6e8fb7db6627beb065e0296f49b66e
Bayological <6872903+bayological@users.noreply.github.com>
2025-10-01 14:29:18 -0500

    chore: disable cpu profiling

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,15 +1,11 @@
 import * as Sentry from '@sentry/nestjs';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   release: process.env.RELEASE_VERSION || 'unknown',
   environment: process.env.SENTRY_ENVIRONMENT || process.env.NODE_ENV || 'production',
-  integrations: [nodeProfilingIntegration()],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
   enabled: process.env.NODE_ENV !== 'development',
   beforeSend(event) {
     // Add release information to all events
